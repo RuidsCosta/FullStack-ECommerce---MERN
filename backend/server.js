6ab1847d268a30be31bdc02c68af86e3f2190987
Rui <rui.diogo.costa@hotmail.com>
2020-11-11 10:11:16 +0000

    Adds sign in form

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,35 @@
 import express from 'express';
-import data from './data.js'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+import productRouter from './routers/productRouter.js';
+import userRouter from './routers/userRouter.js';
+
+dotenv.config()
 
 const app = express();
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
 const port = process.env.PORT || 5000
+const MONGODB_URI = 'mongodb://127.0.0.1:27017/amazona'
 
+mongoose.connect(process.env.MONGODB_URL || MONGODB_URI,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+});
 
-app.get('/api/products/:id', (req, res) => {
-    const product = data.products.find((x) => x._id === req.params.id);
-    if(product){
-        res.send(product)
-    }
-    else{
-        res.status(404).send({message: 'Product not Found'})
-    }
-})
+app.use('/api/users', userRouter);
 
-app.get('/api/products', (req, res) => {
-    res.send(data.products)
-})
+app.use('/api/products', productRouter)
 
 app.get('/', (req, res) => {
     res.send('Server is Ready');
+})
+
+app.use((err, req,res, next) => {
+    res.status(500).send({message: err.message});
 })
 
 app.listen(port, () => {
