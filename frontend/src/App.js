784651f5bcb7c817cffa47980938937a1547f2b5
Rui <rui.diogo.costa@hotmail.com>
2021-01-07 19:49:59 +0000

    adds edit function

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { BrowserRouter, Link, Route } from "react-router-dom";
 import { signout } from "./actions/userActions";
+import PrivateRoute from "./components/PrivateRoute";
 import CartScreen from "./screens/CartScreen";
 import HomeScreen from "./screens/HomeScreen";
 import OrderHistoryScreen from "./screens/OrderHistoryScreen";
@@ -81,7 +82,11 @@ function App() {
             component={OrderHistoryScreen}
             exact
           ></Route>
-          <Route path="/profile" component={ProfileScreen} exact></Route>
+          <PrivateRoute
+            path="/profile"
+            component={ProfileScreen}
+            exact
+          ></PrivateRoute>
           <Route path="/" component={HomeScreen} exact></Route>
         </main>
         <footer className="row center">All rights reserved</footer>
